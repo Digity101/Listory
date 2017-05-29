134ce8cca2e992b9ce87258deae2499674080c32
Digity101 <danny.gia.an.thai@gmail.com>
2017-05-29 19:40:05 +0200

    CSS van Joshua en Claire

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 $servername = "localhost";
 $username = "v15groep3";
 $password = "DTMZJCCV";
-$db = "v15groep1_Dummy";
+$db = "v15groep3_Dummy";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $db);
 mysqli_query($conn, "SET NAMES utf8");
@@ -10,5 +12,5 @@ mysqli_query($conn, "SET NAMES utf8");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "<!---Connected successfully--->";
+//echo "<!---Connected successfully--->";
 ?>
